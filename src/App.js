ad85484747c9522c1c4c6627ac5fe66af2ed819d
Imran <imran1510043@gmail.com>
2022-04-09 19:50:07 +0600

    html and javascript portion done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
+import React, { useState } from "react";
+import "./components/styles.css"
+import List from "./components/List";
+import Data from "./components/data";
+
 function App() {
+  const [people, setPeople] = useState(Data)
   return (
-    <div className="">
-      
-    </div>
+    <main>
+      <section>
+        <h3>{people.length} birthdays today</h3>
+        <List people={people} />
+        <button onClick={() => setPeople([])}>Clear All</button>
+      </section>
+    </main>
   );
 }
 
